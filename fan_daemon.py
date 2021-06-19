0725c19f6938161c1e47faccd3608bf78d3ed103
fumiama <s862105088@gmail.com>
2021-06-20 00:12:56 +0800

    genghuan io

--- fan_daemon.py
+++ fan_daemon.py
@@ -4,27 +4,27 @@ from os import chdir, system, getcwd
 from os.path import exists
 
 def led_on():
-    system("echo 1 > /sys/class/gpio/gpio11/value")
+    system("echo 1 > /sys/class/gpio/gpio13/value")
 
 def led_off():
-    system("echo 0 > /sys/class/gpio/gpio11/value")
+    system("echo 0 > /sys/class/gpio/gpio13/value")
 
 if __name__ == "__main__":
     cwd = getcwd()
     chdir("/sys/class/gpio/")
-    if not exists("gpio21"):
-        print("Exporting GPIO 21...")
-        system("echo 21 > export")
-    system("echo out > gpio21/direction")
-    if not exists("gpio11"):
-        print("Exporting GPIO 11...")
-        system("echo 11 > export")
-    system("echo out > gpio11/direction")
-    if exists("gpio21") and exists("gpio11"):
+    if not exists("gpio26"):
+        print("Exporting GPIO 26...")
+        system("echo 26 > export")
+    system("echo out > gpio26/direction")
+    if not exists("gpio13"):
+        print("Exporting GPIO 13...")
+        system("echo 13 > export")
+    system("echo out > gpio13/direction")
+    if exists("gpio26") and exists("gpio13"):
         print("GPIO ports exported")
         chdir(cwd)
-        system("echo 0 > /sys/class/gpio/gpio21/value")
-        system("echo 0 > /sys/class/gpio/gpio11/value")
+        system("echo 0 > /sys/class/gpio/gpio26/value")
+        system("echo 0 > /sys/class/gpio/gpio13/value")
         fan_on = False
         recognize.led_on = led_on
         recognize.led_off = led_off
@@ -32,8 +32,8 @@ if __name__ == "__main__":
             if recognize.recognize():
                 if not fan_on:
                     fan_on = True
-                    system("echo 1 > /sys/class/gpio/gpio21/value")
+                    system("echo 1 > /sys/class/gpio/gpio26/value")
             elif fan_on:
-                system("echo 0 > /sys/class/gpio/gpio21/value")
+                system("echo 0 > /sys/class/gpio/gpio26/value")
                 fan_on = False
             sleep(8)
