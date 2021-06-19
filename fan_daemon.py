25a6687e38046486f9ad7fd9920a54dc026323c4
fumiama <s862105088@gmail.com>
2021-06-19 22:29:33 +0800

    目录问题

--- fan_daemon.py
+++ fan_daemon.py
@@ -1,8 +1,9 @@
 from recognize import recognize
-from os import chdir, system
+from os import chdir, system, getcwd
 from os.path import exists
 
 if __name__ == "__main__":
+    cwd = getcwd()
     chdir("/sys/class/gpio/")
     if not exists("gpio21"):
         print("Exporting GPIO 21...")
@@ -10,10 +11,10 @@ if __name__ == "__main__":
         system("echo out > gpio21/direction")
     if exists("gpio21"):
         print("GPIO 21 exported")
-        chdir("gpio21")
+        chdir(cwd)
         fan_on = False
         while True:
             if recognize() and not fan_on:
                 fan_on = True
-                system("echo 1 > value")
+                system("echo 1 > /sys/class/gpio/gpio21/value")
             system("sleep 1")
