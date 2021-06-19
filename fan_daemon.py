5a501a440fc4a6bea77dcf34aeaef25b19ffa61b
fumiama <s862105088@gmail.com>
2021-06-19 23:57:48 +0800

    13->16

--- fan_daemon.py
+++ fan_daemon.py
@@ -4,10 +4,10 @@ from os import chdir, system, getcwd
 from os.path import exists
 
 def led_on():
-    system("echo 1 > /sys/class/gpio/gpio13/value")
+    system("echo 1 > /sys/class/gpio/gpio16/value")
 
 def led_off():
-    system("echo 0 > /sys/class/gpio/gpio13/value")
+    system("echo 0 > /sys/class/gpio/gpio16/value")
 
 if __name__ == "__main__":
     cwd = getcwd()
@@ -16,15 +16,15 @@ if __name__ == "__main__":
         print("Exporting GPIO 21...")
         system("echo 21 > export")
         system("echo out > gpio21/direction")
-    if not exists("gpio13"):
+    if not exists("gpio16"):
         print("Exporting GPIO 13...")
         system("echo 13 > export")
-        system("echo out > gpio13/direction")
-    if exists("gpio21") and exists("gpio13"):
+        system("echo out > gpio16/direction")
+    if exists("gpio21") and exists("gpio16"):
         print("GPIO ports exported")
         chdir(cwd)
         system("echo 0 > /sys/class/gpio/gpio21/value")
-        system("echo 0 > /sys/class/gpio/gpio13/value")
+        system("echo 0 > /sys/class/gpio/gpio16/value")
         fan_on = False
         recognize.led_on = led_on
         recognize.led_off = led_off
