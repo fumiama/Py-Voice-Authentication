2a3bfdead51f4952d5f147714189397afe6f05b0
fumiama <s862105088@gmail.com>
2021-06-19 23:52:07 +0800

    增长延时

--- recognize.py
+++ recognize.py
@@ -23,7 +23,9 @@ def recognize() -> bool:
                     rate=RATE, input=True,
                     frames_per_buffer=CHUNK)
 
-    if led_on in locals().keys(): led_on()
+    if led_on in locals().keys():
+        led_on()
+        print("Turn on led.")
     time.sleep(2.0)
     print("recording...")
     frames = []
@@ -32,7 +34,9 @@ def recognize() -> bool:
         data = stream.read(CHUNK, exception_on_overflow=False)
         frames.append(data)
     print("finished recording")
-    if led_off in locals().keys(): led_off()
+    if led_off in locals().keys():
+        led_off()
+        print("Turn off led.")
 
 
     # stop Recording
